1af46927f9a5fb5e3c983f739d0d0c2f044c8430
Akhand Raj Singh <akhandrajsingh30@gmail.com>
2025-10-05 16:48:33 +0530

    Add implementation of linked list reversal and print functions

---
+++ 12_LinkedList/08_reverseLL.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node *next;
+
+    Node(int value) {
+        data = value;
+        next = NULL;
+    }
+};
+
+void reverseLL(Node *&head,Node *&tail){
+    Node *prev = NULL;
+    Node *current = head;
+    tail = current;
+    while(current != 0){
+        Node *forward = current->next;
+        current->next = prev;
+        prev = current;
+        current = forward;
+    }
+    head = prev;
+}
+
+void print(Node *&head){
+     Node *temp = head;
+    cout << "Linked List: ";
+    while (temp != NULL) {
+        cout << temp->data << " -> ";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
+
+int main() {
+    
+    Node *first = new Node(10);
+    Node *second = new Node(20);
+    Node *third = new Node(30);
+    Node *fourth = new Node(40);
+    Node *fifth = new Node(50);
+
+
+    first->next = second;
+    second->next = third;
+    third->next = fourth;
+    fourth->next = fifth;
+    fifth->next = NULL;
+
+    Node *head = first;
+    Node *tail = fifth;
+
+    print(head);
+    
+    reverseLL(head,tail);
+    
+    print(head);
+
+    return 0;
+}
