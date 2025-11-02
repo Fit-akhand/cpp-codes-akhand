b6649b7fbfee51f93285956f24e25c03d0b1c35f
Akhand Raj Singh <akhandrajsingh30@gmail.com>
2025-11-02 11:03:30 +0530

    Implement function to reverse a queue using a stack

---
+++ 14_Queue/06_reverseQueueByStack.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<stack>
+#include<queue>
+using namespace std;
+
+void reverse(queue<int> &q){
+    stack<int> s;
+    while(!q.empty()){
+        s.push(q.front());
+        q.pop();
+    }
+    while(!s.empty()){
+        q.push(s.top());
+        s.pop();
+    }
+}
+
+void print(queue<int> q){
+    while(!q.empty()){
+        cout << q.front() << " " ;
+        q.pop();
+    }
+    cout << endl;
+}
+
+int main(){
+
+    queue<int> q;
+
+    q.push(1);
+    q.push(2);
+    q.push(3);
+    q.push(4);
+    q.push(5);
+
+    print(q);
+    
+    reverse(q);
+
+    print(q);
+
+
+    return 0;
+}
