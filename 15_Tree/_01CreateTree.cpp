1eddab3ad888d9232b904c3855b47e37cc0664be
Akhand Raj Singh <akhandrajsingh30@gmail.com>
2025-11-25 10:52:38 +0530

    Add Preorder (NLR) traversal implementation for binary tree

---
+++ 15_Tree/_01CreateTree.cpp
@@ -0,0 +1,91 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int value){
+        data = value;
+        left = NULL;
+        right = NULL;
+    }
+
+};
+
+// return root node of tree
+Node*buildTree(){
+    int val;
+    cin >> val;
+    if(val == -1){
+        return NULL;
+    }
+    else{
+        Node *root = new Node(val);
+        
+        root->left = buildTree() ;
+        root-> right = buildTree();
+    }
+}
+
+// NLR pre order traversal
+void preOrderTraversal(Node*root){
+    //base case
+    if(root == NULL) {
+        return;
+    }
+    // ek case mai solve karunga baki recursion sambhal lega
+    // N
+    cout << root->data << " ";
+    // L
+    preOrderTraversal(root->left);
+    // R
+    preOrderTraversal(root->right);
+}
+
+void inOrderTraversal(Node*root){
+    //base case
+    if(root == NULL) {
+        return;
+    }
+    // ek case mai solve karunga baki recursion sambhal lega
+    // L
+    inOrderTraversal(root->left);
+    // N
+    cout << root->data << " ";
+    // R
+    inOrderTraversal(root->right);
+}
+
+void postOrderTraversal(Node*root){
+    //base case
+    if(root == NULL) {
+        return;
+    }
+    // ek case mai solve karunga baki recursion sambhal lega
+    // L
+    postOrderTraversal(root->left);
+    // R
+    postOrderTraversal(root->right);
+    // N
+    cout << root->data << " ";
+}
+
+int main(){
+
+    // 10 20 30 -1 -1 40 -1 -1 50 -1 60 -1 -1
+    Node* root  = buildTree();
+    cout << endl <<" print Preorder tracersal : " << endl;
+    preOrderTraversal(root);
+    
+    cout <<  endl <<" print InOrderTraversal tracersal : " << endl;
+    inOrderTraversal(root);
+   
+    cout <<  endl <<" print PostOrderTraversal tracersal : " << endl;
+    postOrderTraversal(root);
+
+
+    return 0;
+}
